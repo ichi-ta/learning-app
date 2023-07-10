261f3030f1af9b5b34a00ea6c0515b24c0cbf54b
ichita <gogoishin0911@gmail.com>
2023-07-10 14:52:18 +0900

    セット作成フォームの作成

--- app/src/sets/views.py
+++ app/src/sets/views.py
@@ -2,17 +2,19 @@ from flask import Blueprint, render_template, redirect, request, url_for
 from flask_login import current_user
 
 from app import app
-from app.models import QuestionSet
+from app.models import db, QuestionSet
 
 sets = Blueprint("sets",__name__)
 
 @sets.route("/sets", methods=["GET"])
 def sets_get():
-  set = QuestionSet.query.all(current_user)
+  set = QuestionSet.query.all()
   return render_template("sets/sets_get.html",sets=set)
 
 @sets.route("/sets", methods=["POST"])
 def sets_post():
   set = QuestionSet(name=request.form["set_name"])
-  return render_template("sets/sets_post")
+  db.session.add(set)
+  db.session.commit()
+  return redirect(url_for('sets.sets_get'))
   
