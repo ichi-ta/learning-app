35be5f79b36b4a12e909cd2b388deba2e90bbfd6
ichita <gogoishin0911@gmail.com>
2023-07-17 14:15:32 +0900

    ログイン機能の改善

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_login import current_user
 
 app = Flask(__name__)
 
-#from app.src.users.views import users
+@app.route("/", methods=["GET"])
+def top_get():
+  if current_user.is_authenticated:
+    return render_template("top.html")
+  return redirect(url_for("logins.login_post"))
+
 from app.src.logins.views import logins
 
-#app.register_blueprint(users)
 app.register_blueprint(logins)
