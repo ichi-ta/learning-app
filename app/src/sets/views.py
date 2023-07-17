f8caeda5f15882bbb7aebba2e91642a1b7982a66
ichita <gogoishin0911@gmail.com>
2023-07-17 11:09:27 +0900

    削除機能の実装

--- app/src/sets/views.py
+++ app/src/sets/views.py
@@ -6,14 +6,16 @@ from app.models import db, QuestionSet
 
 sets = Blueprint("sets",__name__)
 
+#今ログインしているユーザの学習セット一覧
 @sets.route("/sets", methods=["GET"])
 def sets_get():
+  #学習セットのuser_idと今ログインしているユーザのidが等しいセットを取得
   sets = QuestionSet.query.filter(QuestionSet.user_id == current_user.id).all()
   return render_template("sets/sets_get.html",sets=sets)
 
 #学習セット作成フォームの処理
 @sets.route("/sets", methods=["POST"])
-def sets_post():
+def set_post():
   set = QuestionSet(
     name=request.form["set_name"],
     user_id=current_user.id
@@ -22,8 +24,16 @@ def sets_post():
   db.session.commit()
   return redirect(url_for('sets.sets_get'))
 
+#学習セット削除の処理
+@sets.route("/sets/<set_id>/delete", methods=["POST"])
+def set_delete(set_id):
+  set = QuestionSet.query.filter(QuestionSet.user_id==current_user.id, QuestionSet.id==set_id).first()
+  db.session.delete(set)
+  db.session.commit()
+  return redirect(url_for('sets.sets_get'))
+
 #学習セット詳細ページの処理
 @sets.route("/sets/<set_id>",methods=["GET"])
-def sets_detail(set_id):
+def set_detail(set_id):
   set = QuestionSet.query.filter(QuestionSet.user_id==current_user.id, QuestionSet.id==set_id).first()
   return render_template("sets/set_detail.html", set=set)
