813bbd5fe46dbbe9891029d860ecd4197891c597
ichita <gogoishin0911@gmail.com>
2023-07-17 14:52:32 +0900

    ログイン機能の実装

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/top",methods=["GET"])
+@login_required
 def top_get():
-  if current_user.is_authenticated:
-    return render_template("top.html")
-  return redirect(url_for("logins.login_post"))
+  return render_template("top.html")
 
 from app.src.logins.views import logins
 from app.src.users.views import users
--- app/src/logins/views.py
+++ app/src/logins/views.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from flask_login import login_user, logout_user
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
+from app import app
 from app.models import User
 
 logins = Blueprint("logins", __name__)
+login = LoginManager(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@logins.route('/login', methods=['GET'])
+def login_get():
+  if current_user.is_authenticated:
+    return redirect(url_for('top_get'))
+  return render_template('login.html')
 
 @logins.route('/login', methods=['POST'])
 def login_post():
